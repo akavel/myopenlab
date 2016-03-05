bac707858e6890f2938d3acf2a62d15cc3cc5a4c
carmelosalafia <carmelosalafia@f32457fc-df50-475e-a8c3-81db5cb348b4>
2016-03-05 15:07:25 +0000

    Version 3.0.4.6

--- src/VisualLogic/Version.java
+++ src/VisualLogic/Version.java
@@ -24,7 +24,7 @@ package VisualLogic;
 public class Version 
 {           
     public static String strApplicationTitle="MyOpenLab";
-    public static String strApplicationVersion="3.0.4.5";
+    public static String strApplicationVersion="3.0.4.6";
     public static String strFileVersion="3.14";
     public static String strStatus="";
 }
